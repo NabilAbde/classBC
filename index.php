5f4a08d3de5f61bc1f04d5f1eb6834979518e939
[nabil] <[consonab@gmail.com]>
2021-06-09 12:13:41 +0100

    creation dispatcher

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+switch ($_SERVER['REQUEST_URI']) {
+    case '/test_class_BC/categories':
+        include 'view/vCategories.php';
+        break;
+    case '/test_class_BC/home':
+        include 'view/home.php';
+        break;
+    case '/test_class_BC/personne':
+        include 'view/vPersonne.php';
+        break;
+    default:
+        include 'view/404.php';
+        break;
+}
